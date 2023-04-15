9149803681914bbe4472450c4ffc3b9a3afa8cff
GoncalomcgSilva <Goncalo_Guerreiro@iscte-iul.pt>
2023-04-15 16:29:48 +0100

    JSON para CSV incompleto

--- src/main/java/Functions.java
+++ src/main/java/Functions.java
@@ -12,11 +12,14 @@ import java.util.logging.Logger;
 
 public class Functions {
 
-    public static void csvToJSON() throws FileNotFoundException {
-        String inputFile = "src/horario_exemplo.csv";
+    private Functions() {
+        throw new IllegalStateException("Aux Functions");
+    }
+
+    public static void csvToJSON(String fileName) throws FileNotFoundException {
         String outputFile = "horarios.json";
         Logger logger = Logger.getLogger(Main.class.getName());
-        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             boolean firstLine = true;
             try (FileWriter fw = new FileWriter(outputFile)) {
@@ -29,7 +32,7 @@ public class Functions {
                     String[] columns = line.split(";", -1);
                     JSONObject sala = new JSONObject();
                     sala.put("Sala", columns[9]);
-                    sala.put("Lotação da sala", columns[10]);
+                    sala.put("Lotação da sala", Integer.parseInt(columns[10]));
                     JSONObject data = new JSONObject();
                     data.put("Data", columns[8]);
                     data.put("Dia da semana", columns[5]);
@@ -57,7 +60,7 @@ public class Functions {
         }
     }
 
-    public static void JSONTocsv() {
+    public static void jsonTocsv() {
         String jsonString;
         JSONObject jsonObject;
         try {
@@ -69,6 +72,44 @@ public class Functions {
             FileUtils.writeStringToFile(file, csvString);
         }
         catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //implementação gonçalo
+    public static void convertJsonToCsv(String fileName) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String fileInString = "";
+            String line;
+            while ((line = reader.readLine()) != null) {
+                fileInString += line;
+            }
+            reader.close();
+
+            JSONArray jsonArray = new JSONArray(fileInString);
+
+            FileWriter writer = new FileWriter("horario.csv");
+            writer.append("Curso;Unidade Curricular;Turno;Turma;Inscritos no turno;Dia da semana;Hora início da aula;Hora fim da aula;Data da aula;Sala atribuída à aula;Lotação da sala\n");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                JSONObject turno = jsonObject.getJSONObject("Unidade Curricular").getJSONObject("Turno");
+                String unidadeCurricular = jsonObject.getJSONObject("Unidade Curricular").getString("Designação");
+                String curso = jsonObject.getJSONObject("Unidade Curricular").getJSONArray("Curso").getString(0);
+                String turnoId = turno.getString("Id Turno");
+                String turma = turno.getJSONArray("Turma").getString(0);
+                int inscritos = turno.getInt("Inscritos no turno");
+                String horaInicio = turno.getJSONObject("Data da aula").getString("Hora início da aula");
+                String horaFim = turno.getJSONObject("Data da aula").getString("Hora fim da aula");
+                String data = turno.getJSONObject("Data da aula").getString("Data");
+                String diaSemana = turno.getJSONObject("Data da aula").getString("Dia da semana");
+                int lotacaoSala = turno.getJSONObject("Data da aula").getJSONObject("Sala atribuída à aula").getInt("Lotação da sala");
+                String sala = turno.getJSONObject("Data da aula").getJSONObject("Sala atribuída à aula").getString("Sala");
+                writer.append(String.format("%s;%s;%s;%s;%d;%s;%s;%s;%s;%s;%d%n", curso, unidadeCurricular, turnoId, turma, inscritos, diaSemana, horaInicio, horaFim, data, sala, lotacaoSala));
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException | JSONException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Main{
     public static void main(String[] args) {
-//        Functions.csvToJSON();
-        Functions.JSONTocsv();
+//        Functions.csvToJSON("src/horario_exemplo.csv");
+        Functions.convertJsonToCsv("horarios.json");
     }
 }
