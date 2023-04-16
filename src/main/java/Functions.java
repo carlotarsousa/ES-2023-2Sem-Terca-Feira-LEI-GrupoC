508631e4ec40db4223b229c13d8e36353d416671
GoncalomcgSilva <Goncalo_Guerreiro@iscte-iul.pt>
2023-04-16 16:54:58 +0100

    code smells corrigidos

--- src/main/java/Functions.java
+++ src/main/java/Functions.java
@@ -5,8 +5,8 @@ import org.json.CDL;
 import org.apache.commons.io.FileUtils;
 
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -14,7 +14,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Functions {
-
+    private static final String SALA = "Sala atribuída à aula";
+    private static final String UC = "Unidade Curricular";
+    private static final String DATA = "Data da aula";
     private Functions() {
         throw new IllegalStateException("Aux Functions");
     }
@@ -22,45 +24,46 @@ public class Functions {
     public static void csvToJSON(String loadFilePath, String saveFilePath) throws FileNotFoundException {
         Logger logger = Logger.getLogger(Main.class.getName());
         try (BufferedReader br = loadFile(loadFilePath)) {
-            String line;
-            JSONArray jsonArray = new JSONArray();
-            boolean firstLine = true;
-            while ((line = br.readLine()) != null) {
-                logger.log(Level.INFO, line);
-                if (firstLine) {
-                    firstLine = false;
-                    continue;
+            if(br != null){
+                String line;
+                JSONArray jsonArray = new JSONArray();
+                boolean firstLine = true;
+                while ((line = br.readLine()) != null) {
+                    logger.log(Level.INFO, line);
+                    if (firstLine) {
+                        firstLine = false;
+                        continue;
+                    }
+
+                    String[] columns = line.split(";", -1);
+
+                    JSONObject sala = new JSONObject();
+                    sala.put("Sala", columns[9]);
+                    sala.put("Lotação da sala", columns[10]);
+                    JSONObject data = new JSONObject();
+                    data.put("Data", columns[8]);
+                    data.put("Dia da semana", columns[5]);
+                    data.put("Hora início da aula", columns[6]);
+                    data.put("Hora fim da aula", columns[7]);
+                    data.put(SALA, sala);
+                    JSONObject turno = new JSONObject();
+                    turno.put("Id Turno", columns[2]);
+                    turno.put("Turma", columns[3].split(",\\s*"));
+                    turno.put("Inscritos no turno", Integer.parseInt(columns[4]));
+                    turno.put(DATA, data);
+                    JSONObject unidadeCurricular = new JSONObject();
+                    unidadeCurricular.put("Designação", columns[1]);
+                    unidadeCurricular.put("Curso", columns[0].split(",\\s*"));
+                    unidadeCurricular.put("Turno", turno);
+                    JSONObject horario = new JSONObject();
+                    horario.put(UC, unidadeCurricular);
+                    jsonArray.put(horario);
                 }
 
-                String[] columns = line.split(";", -1);
-
-                JSONObject sala = new JSONObject();
-                sala.put("Sala", columns[9]);
-                sala.put("Lotação da sala", columns[10]);
-                JSONObject data = new JSONObject();
-                data.put("Data", columns[8]);
-                data.put("Dia da semana", columns[5]);
-                data.put("Hora início da aula", columns[6]);
-                data.put("Hora fim da aula", columns[7]);
-                data.put("Sala atribuída à aula", sala);
-                JSONObject turno = new JSONObject();
-                turno.put("Id Turno", columns[2]);
-                turno.put("Turma", columns[3].split(",\\s*"));
-                turno.put("Inscritos no turno", Integer.parseInt(columns[4]));
-                turno.put("Data da aula", data);
-                JSONObject unidadeCurricular = new JSONObject();
-                unidadeCurricular.put("Designação", columns[1]);
-                unidadeCurricular.put("Curso", columns[0].split(",\\s*"));
-                unidadeCurricular.put("Turno", turno);
-                JSONObject horario = new JSONObject();
-                horario.put("Unidade Curricular", unidadeCurricular);
-                jsonArray.put(horario);
-            }
-
-            try (BufferedWriter bw = saveFile(saveFilePath)) {
-                if(bw != null) {
-                    bw.write(jsonArray.toString(4));
-                    bw.close();
+                try (BufferedWriter bw = saveFile(saveFilePath)) {
+                    if(bw != null) {
+                        bw.write(jsonArray.toString(4));
+                    }
                 }
             }
 
@@ -89,31 +92,31 @@ public class Functions {
     public static void convertJsonToCsv(String fileName) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            String fileInString = "";
+            StringBuilder fileInString = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
-                fileInString += line;
+                fileInString.append(line);
             }
             reader.close();
 
-            JSONArray jsonArray = new JSONArray(fileInString);
+            JSONArray jsonArray = new JSONArray(fileInString.toString());
 
             FileWriter writer = new FileWriter("horario.csv");
             writer.append("Curso;Unidade Curricular;Turno;Turma;Inscritos no turno;Dia da semana;Hora início da aula;Hora fim da aula;Data da aula;Sala atribuída à aula;Lotação da sala\n");
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
-                JSONObject turno = jsonObject.getJSONObject("Unidade Curricular").getJSONObject("Turno");
-                String unidadeCurricular = jsonObject.getJSONObject("Unidade Curricular").getString("Designação");
-                String curso = jsonObject.getJSONObject("Unidade Curricular").getJSONArray("Curso").getString(0);
+                JSONObject turno = jsonObject.getJSONObject(UC).getJSONObject("Turno");
+                String unidadeCurricular = jsonObject.getJSONObject(UC).getString("Designação");
+                String curso = jsonObject.getJSONObject(UC).getJSONArray("Curso").getString(0);
                 String turnoId = turno.getString("Id Turno");
                 String turma = turno.getJSONArray("Turma").getString(0);
                 int inscritos = turno.getInt("Inscritos no turno");
-                String horaInicio = turno.getJSONObject("Data da aula").getString("Hora início da aula");
-                String horaFim = turno.getJSONObject("Data da aula").getString("Hora fim da aula");
-                String data = turno.getJSONObject("Data da aula").getString("Data");
-                String diaSemana = turno.getJSONObject("Data da aula").getString("Dia da semana");
-                int lotacaoSala = turno.getJSONObject("Data da aula").getJSONObject("Sala atribuída à aula").getInt("Lotação da sala");
-                String sala = turno.getJSONObject("Data da aula").getJSONObject("Sala atribuída à aula").getString("Sala");
+                String horaInicio = turno.getJSONObject(DATA).getString("Hora início da aula");
+                String horaFim = turno.getJSONObject(DATA).getString("Hora fim da aula");
+                String data = turno.getJSONObject(DATA).getString("Data");
+                String diaSemana = turno.getJSONObject(DATA).getString("Dia da semana");
+                int lotacaoSala = turno.getJSONObject(DATA).getJSONObject(SALA).getInt("Lotação da sala");
+                String sala = turno.getJSONObject(DATA).getJSONObject(SALA).getString("Sala");
                 writer.append(String.format("%s;%s;%s;%s;%d;%s;%s;%s;%s;%s;%d%n", curso, unidadeCurricular, turnoId, turma, inscritos, diaSemana, horaInicio, horaFim, data, sala, lotacaoSala));
             }
             writer.flush();
@@ -124,18 +127,20 @@ public class Functions {
     }
     private static BufferedReader loadFile(String filePath) {
         try {
-            URL url = new URL(filePath);
+            URI uri = new URI(filePath);
+            URL url = uri.toURL();
             return new BufferedReader(new InputStreamReader(url.openStream()));
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
         return null;
     }
     private static BufferedWriter saveFile(String filePath) {
         try {
-            URL url = new URL(filePath);
+            URI uri = new URI(filePath);
+            URL url = uri.toURL();
             return new BufferedWriter(new OutputStreamWriter(url.openConnection().getOutputStream()));
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
         return null;
